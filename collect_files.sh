9faeccc7605f29fa21c4067790e65328454dd239
Your Name <you@example.com>
2025-04-28 18:55:59 +0300

    att5

--- collect_files.sh
+++ collect_files.sh
@@ -14,10 +14,7 @@ input_dir = "$input_dir"
 output_dir = "$output_dir"
 
 for dirpath, _, filenames in os.walk(input_dir):
-    relative_path = os.path.relpath(dirpath, input_dir)
-    level = relative_path.count(os.sep)
-    if level < 2: 
-        for file in filenames:
-            shutil.copy2(dirpath + "/" + file, output_dir)
+    for file in filenames:
+        shutil.copy2(dirpath + "/" + file, output_dir)
 
 END
