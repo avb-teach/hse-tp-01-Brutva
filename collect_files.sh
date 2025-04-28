53e0a76a321623104156e46413a3f5237f110008
Your Name <you@example.com>
2025-04-28 18:31:08 +0300

    task 2

--- collect_files.sh
+++ collect_files.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+input_dir=$1
+output_dir=$2
+
+mkdir $output_dir
+
+python3 - <<END
+
+import shutil
+import os
+
+input_dir = $input_dir
+output_dir = $output_dir
+
+for dirpath, _, filenames in os.walk(source):
+    relative_path = os.path.relpath(dirpath, source)
+    level = relative_path.count(os.sep)
+    if level < 2: 
+        for file in filenames:
+            shutil.copy2(dirpath + "/" + file, destination)
+
+END
